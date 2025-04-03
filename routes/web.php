d90941804a7e486658a3e0c7c701b71abf1c8df3
LENOVO <yogatiarawiguna@gmail.com>
2025-04-03 12:17:28 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/gallery', function () {
+  return view('home');
+});
+
+Route::get('/gallery', function () {
+  return view('home');
+});
